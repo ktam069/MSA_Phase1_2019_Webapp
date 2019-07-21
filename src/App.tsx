295b5fc3092735deedc0cc4eaaa95cc0df9fd9e5
ktam069 <ktam069@aucklanduni.ac.nz>
2019-07-21 16:05:13 +1200

    Fixed some errors.

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,35 @@
 import * as React from 'react';
+import DropArea from './components/droparea/DropArea';
 import Header from './components/header/Header';
-import DropArea from './Components/droparea/DropArea';
-import Result from './Components/result/Result';
+import Result from './components/result/Results';
 
-class App extends React.Component {
-  interface IState {
-    result:string
-    filelength:number
-  }
-  
-  class App extends React.Component<{},IState>{
-      public constructor(props:any){
-        super(props)
-        this.state = {
-          result:"",
-          filelength:0,
-        }
-      }
+interface IState {
+  result:string
+  filelength:number
+}
 
-      public resultstate = (resultString:string,filelen:any) => {
-        this.setState({result:resultString,filelength:filelen})
+class App extends React.Component<{},IState>{
+    public constructor(props:any){
+      super(props)
+      this.state = {
+        filelength:0,
+        result:"",
       }
+    }
 
-      public render() {
-        return (
-          <div>
-            <Header />
-            <DropArea setResults={this.resultstate} />
-            <Result result={this.state.result} filelength={this.state.filelength} />
-          </div>
-        );
-      }
+    public resultstate = (resultString:string,filelen:any) => {
+      this.setState({result:resultString,filelength:filelen})
+    }
+
+    public render() {
+      return (
+        <div>
+          <Header />
+          <DropArea setResults={this.resultstate} />
+          <Result result={this.state.result} filelength={this.state.filelength} />
+        </div>
+      );
+    }
 }
 
 export default App;
